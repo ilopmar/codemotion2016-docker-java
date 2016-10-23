84598f2110a79b79de2ebf89f24c4f0d874d27df
Iván López <lopez.ivan@gmail.com>
2016-10-23 17:02:55 +0200

    Producer: Send messages to RabbitMQ

--- producer/src/main/java/codemotion2016/ProducerApplication.java
+++ producer/src/main/java/codemotion2016/ProducerApplication.java
@@ -1,12 +1,43 @@
 package codemotion2016;
 
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @SpringBootApplication
 public class ProducerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerApplication.class, args);
-	}
+    private final static String queueName = "reverse";
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @RestController
+    class ProducerController {
+
+        @Autowired
+        RabbitTemplate rabbitTemplate;
+
+        @RequestMapping(method = RequestMethod.GET, path = "/producer")
+        public String sendMsg(Optional<String> msg) {
+            String messageSent = msg.orElse("Hola Codemotion");
+            rabbitTemplate.convertAndSend(ProducerApplication.queueName, messageSent);
+
+            return "Mensaje enviado = " + messageSent;
+        }
+    }
 }
